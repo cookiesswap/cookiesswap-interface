c753c66aa4b560d64b5ad81296b15474d7a526fd
Cryptocode <crytocode99@gmail.com>
2021-03-24 14:47:38 +0700

    Initial commit cookiesswap

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,4 +1,4 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'retjehnetwork.bsc'
+export const DEFAULT_TOKEN_LIST_URL = 'cookiesswap.bsc'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [DEFAULT_TOKEN_LIST_URL]
--- src/state/lists/reducer.ts
+++ src/state/lists/reducer.ts
@@ -4,7 +4,7 @@ import { TokenList } from '@uniswap/token-lists/dist/types'
 import { DEFAULT_LIST_OF_LISTS, DEFAULT_TOKEN_LIST_URL } from '../../constants/lists'
 import { updateVersion } from '../global/actions'
 import { acceptListUpdate, addList, fetchTokenList, removeList, selectList } from './actions'
-import DEFAULT_LIST from '../../constants/token/retjehnetwork.bsc.json'
+import DEFAULT_LIST from '../../constants/token/cookiesswap.bsc.json'
 
 export interface ListsState {
   readonly byUrl: {
